5ffb9eec5acf0c61e5375cbad27187e6539d69f2
kenshivr <Aaladar@proton.me>
2025-06-18 15:03:08 -0600

    Test svg technologies.

--- src/svg/css.tsx
+++ src/svg/css.tsx
@@ -1,7 +1,7 @@
 export default function CssLogo({ className = "w-28 h-28" }: { className?: string }) {
   return (
     <img
-      src="/Brayan/public/svg/css-svgrepo-com.svg"
+      src="/Brayan/svg/css-svgrepo-com.svg"
       alt="Css Logo"
       className={className}
     />
